d2a165395dc649253c52375fad6ff02e69fcacad
pooyan saeedinia <pooyansaeedinia@gmail.com>
2026-07-30 17:16:57 +0300

    Recreate requirements.txt

--- MEDIFIT/settings.py
+++ MEDIFIT/settings.py
@@ -28,7 +28,7 @@ DEBUG = config("DEBUG", cast=bool, default=False)
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    "https://medifit-7a8i.onrender.com"
+    ".onrender.com"
 ]
 
 # Application definition
